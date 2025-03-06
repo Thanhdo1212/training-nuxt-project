345e53e91cd565218bb15eb544897b44fb67bd28
Thanhdo1212 <thanhdo12122001@gmail.com>
2025-03-06 17:27:22 +0700

    fix: fix detail button

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -68,12 +68,15 @@
                   >
                   <v-card-subtitle>{{ product?.price }}</v-card-subtitle>
                   <v-card-actions class="d-flex justify-space-between">
-                    <v-btn color="orange-lighten-2" @click="openDialog(product)"
+                    <v-btn
+                      color="orange-lighten-2"
+                      @click="router.push({ path: `/products/${product.id}` })"
                       >Detail</v-btn
                     >
                     <v-btn color="blue-lighten-2" @click="openDialog(product)"
                       >View</v-btn
                     >
+                  </v-card-actions>
                 </v-card>
               </v-col>
             </v-row>
